dd4081dff1bc12096ebb79231bbb8370c44d06e0
lbrendanl <lee33@uw.edu>
2016-03-10 18:16:49 -0800

    Initial commit of multi-table in simulator.  Works in basic scenarios, still needs cleanup, some docs, and incremental refresh scenarios.

---
+++ js/convert-connector.js
@@ -0,0 +1,54 @@
+function _convertConnectorFor12(connector) {
+  var _schemaCallback = null;
+  // The old connector will have 2 functions, getColumnHeaders and getTableData. Lets convert them
+  connector.getSchema = function(schemaCallback) {
+  	_schemaCallback = schemaCallback;
+  	connector.getColumnHeaders();
+  }
+
+  tableau.headersCallback = function(fieldNames, types) {
+  	// Convert this into a table info object
+  	var cols = [];
+  	for(var i=0; i<fieldNames.length && i<types.length; i++) {
+  		var colInfo = {
+  			id: fieldNames[i],
+  			dataType: types[i]
+  		};
+
+  		cols.push(colInfo);
+  	}
+
+  	var tableInfo = {
+  	  id : "WDC",
+  	  incrementColumnId: tableau.incrementalExtractColumn,
+  	  columns : cols
+  	};
+
+  	_schemaCallback([tableInfo]);
+  }
+
+  var _dataDoneCallback;
+  var _table;
+
+  connector.getData = function(tableCollection, dataDoneCallback) {
+  	_dataDoneCallback = dataDoneCallback;
+
+  	// There should only be 1 table in our tableCollection
+  	_table = tableCollection.getTables()['WDC'];
+  	connector.getTableData(_table.incrementValue);
+  }
+
+  tableau.dataCallback = function(data, lastRecordToken, moreData) {
+  	// We got some data back from the connector itself. Call back with the data first
+  	_table.appendRows(data);
+  	if (moreData) {
+  	  // If we have more data we need to simulate what Tableau used to do and run the for loop
+  	  // Do it via window.setTimeout to help with the stack
+  	  window.setTimeout(function() {
+  	  	connector.getTableData(lastRecordToken);
+  	  }, 0);
+  	} else {
+  	  _dataDoneCallback();
+  	}
+  }
+}
