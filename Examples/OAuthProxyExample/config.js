ab7d2b0316c0c8c2cb77226060bdf8d69a3f38d7
lbrendanl <lee33@uw.edu>
2016-04-06 16:45:14 -0700

    Added OAuthProxyExample.

---
+++ Examples/OAuthProxyExample/config.js
@@ -0,0 +1,10 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'YOUR_CLIENT_ID',
+ 'CLIENT_SECRET': 'YOUR_CLIENT_SECRET',
+ 'REDIRECT_PATH': '/redirect'
+};
