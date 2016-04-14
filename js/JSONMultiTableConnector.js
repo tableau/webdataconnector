4e4a7130c619fba564cc242bb3f1d34b788adda5
lbrendanl <lee33@uw.edu>
2016-04-14 13:19:09 -0700

    Refactored JSON sample to read from local json file.

--- js/JSONMultiTableConnector.js
+++ js/JSONMultiTableConnector.js
@@ -2,75 +2,11 @@
     var myConnector = tableau.makeConnector();
     
     myConnector.getSchema = function(schemaCallback) {
-         var userCols = [
-             { id: "id", dataType: "int" },
-             { id: "name", dataType: "string" },
-             { id: "username", dataType: "string" },
-             { id: "phone", dataType: "string" },
-             { id: "website",  dataType: "string" },
-         ];
-         
-         var postCols = [
-             { id: "id", dataType: "int" },
-             { id: "userId", dataType: "int" },
-             { id: "title", dataType: "string" },
-             { id: "body", dataType: "string" }
-         ];
-         
-         var commentCols = [
-             { id: "id", dataType: "int" },
-             { id: "postId", dataType: "int" },
-             { id: "name", dataType: "string" },
-             { id: "email", dataType: "string" },
-             { id: "body",  dataType: "string" },
-         ];
-         
-         var albumCols = [
-             { id: "id", dataType: "int" },
-             { id: "userId", dataType: "int" },
-             { id: "title", dataType: "string" }
-         ];
-         
-         var photoCols = [
-             { id: "id", dataType: "int" },
-             { id: "albumId", dataType: "int" },
-             { id: "title", dataType: "string" },
-             { id: "url", dataType: "string" },
-             { id: "thumbnailUrl",  dataType: "string" },
-         ];
-         
-         var usersTable = {
-             alias: "Users",
-             id: "users",
-             columns: userCols
-         };
-         
-         var postsTable = {
-             alias: "Posts",
-             id: "posts",
-             columns: postCols
-         };
-         
-         var commentsTable = {
-             alias: "Comments",
-             id: "comments",
-             columns: commentCols
-         };
-         
-         var albumsTable = {
-             alias: "Albums",
-             id: "albums",
-             columns: albumCols
-         };
-         
-         var photosTable = {
-             alias: "Photos",
-             id: "photos",
-             columns: photoCols
-         };
-         
-         schemaCallback([usersTable, postsTable, commentsTable, albumsTable, photosTable]);
-     };
+        // In this sample, we read our schema definition from a local JSON file.
+        $.getJSON("../js/JSONMultiTableData.json", function(schemaJson) {
+            schemaCallback(schemaJson);
+        });
+    };
 
     myConnector.getData = function(table, doneCallback) {
         var apiURL = 'http://jsonplaceholder.typicode.com/' + table.tableInfo.id;
