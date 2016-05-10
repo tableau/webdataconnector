54e1600ee1b6f058ece1911d126090adf6356261
Jared Dominguez <jdominguez@tableau.com>
2016-05-09 20:06:11 -0700

    Fix indent for new sample WDC

--- js/earthquakeUSGS.js
+++ js/earthquakeUSGS.js
@@ -1,19 +1,19 @@
 (function () {
-    // Create the connector object
+	// Create the connector object
 	var myConnector = tableau.makeConnector();
 
-    // Define the schema
+	// Define the schema
 	myConnector.getSchema = function (schemaCallback) {
 		var cols = [{
 				id : "mag",
 				alias : "magnitude",
 				dataType : "float"
-			}, 
+			},
             {
-                id : "title",
+				id : "title",
 				alias : "title",
 				dataType : "string"
-			}, 
+			},
             {
 				id : "url",
 				alias : "url",
@@ -40,43 +40,43 @@
 		schemaCallback([tableInfo]);
 	};
 
-    // Download the data
+	// Download the data
 	myConnector.getData = function (table, doneCallback) {
-        var tableData = [],
-            mag = 0,
-            title = "",
-            url = "",
-            lat = "",
-            lon = "";
+		var tableData = [],
+		mag = 0,
+		title = "",
+		url = "",
+		lat = "",
+		lon = "";
 
-        $.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features;
+		$.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function (resp) {
+			var feat = resp.features;
 
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                mag = feat[i].properties.mag;
-                title = feat[i].properties.title;
-                url = feat[i].properties.url;
-                lon = feat[i].geometry.coordinates[0];
-                lat = feat[i].geometry.coordinates[1];
+			// Iterate over the JSON object
+			for (var i = 0, len = feat.length; i < len; i++) {
+				mag = feat[i].properties.mag;
+				title = feat[i].properties.title;
+				url = feat[i].properties.url;
+				lon = feat[i].geometry.coordinates[0];
+				lat = feat[i].geometry.coordinates[1];
 
-                tableData.push({ 
-                   "mag": mag,
-                   "title": title,
-                   "url": url,
-                   "lon": lon,
-                   "lat": lat
-                });
-            }
+				tableData.push({
+					"mag" : mag,
+					"title" : title,
+					"url" : url,
+					"lon" : lon,
+					"lat" : lat
+				});
+			}
 
-            table.appendRows(tableData);
-            doneCallback();
-        });
-    };
+			table.appendRows(tableData);
+			doneCallback();
+		});
+	};
 
 	setupConnector = function () {
-        tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-        tableau.submit(); // This sends the connector object to Tableau
+		tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+		tableau.submit(); // This sends the connector object to Tableau
 	};
 
 	tableau.registerConnector(myConnector);
