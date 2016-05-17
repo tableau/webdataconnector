5026c69b6f3f57046dc567f3667573777c1d877b
lbrendanl <lee33@uw.edu>
2016-05-17 12:28:05 -0700

    Moved community connectors from README to Community Zone.

--- js/community_connectors.js
+++ js/community_connectors.js
@@ -1,14 +1,27 @@
 function append(connector){
-    var grid = document.querySelector('#columns');
+    var grid = document.querySelector('#grid');
     var item = document.createElement('div');
     
     var h = '<div>';
         h += '<div class="thumbnail">';
-        h += '<a href="' + connector.url + '"><img src="' + connector.thumbnail_path + '" alt="Connector Link"></a>'
+        h += '<div class="connector_title">'
+        h += '<h2><a href="' + connector.url + '" alt="Connector Link">' + connector.name + '</a>'
+        h += '</div>';
         h += '<div class="caption">';
-        h += '<p><h5>' + connector.name + '</h5></p>';
-        h += '<p>Written by: ' + connector.author + '</p>';
-        //h += '<p>' + connector.tags + '</p>';
+        
+        if (connector.github_username) {
+            h += '<p><b>Written by: </b><a href="https://github.com/' + connector.github_username + '" alt="GH Link">' + connector.author + '</a></p>';
+        } else {
+            h += '<p><b>Written by: </b>' + connector.author + '</p>';
+        }
+        
+        if (connector.source_code) {
+            h += '<p><a href="' + connector.source_code + '" alt="Source code link">Source Code Available</a></p>';
+        }
+        
+        if (connector.description) {
+            h += '<p><b>Description: </b>' + connector.description + '</p>';
+        }
         
         connector.tags.forEach(function(tag) {
             h += '<span class="label label-pill label-primary">' + tag + '</span>' 
@@ -23,6 +36,6 @@ function append(connector){
 
 $.getJSON("./community_connectors.json", function(data){
     $(data).each(function(i, connector) {
-        append(connector);
+         append(connector);
     });
 });
