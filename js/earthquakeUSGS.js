978693ee8f28fd82a5d2fbf26a96c7520c9b46cc
Jared Dominguez <jred51@gmail.com>
2016-05-26 22:05:31 -0700

    Clean up indenting of earthquake examples

--- js/earthquakeUSGS.js
+++ js/earthquakeUSGS.js
@@ -1,68 +1,82 @@
-(function () {
-	// Create the connector object
-	var myConnector = tableau.makeConnector();
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
 
-	// Define the schema
-	myConnector.getSchema = function (schemaCallback) {
-		var cols = [
-			{ id : "mag", alias : "magnitude", dataType : tableau.dataTypeEnum.float },
-			{ id : "title", alias : "title", dataType : tableau.dataTypeEnum.string },
-			{ id : "url", alias : "url", dataType : tableau.dataTypeEnum.string },
-			{ id : "lat", alias : "latitude", dataType : tableau.dataTypeEnum.float },
-			{ id : "lon", alias : "longitude", dataType : tableau.dataTypeEnum.float }
-		];
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "mag",
+            alias: "magnitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "title",
+            alias: "title",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "url",
+            alias: "url",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "lat",
+            alias: "latitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "lon",
+            alias: "longitude",
+            dataType: tableau.dataTypeEnum.float
+        }];
 
-		var tableInfo = {
-			id : "earthquakeFeed",
-			alias : "Earthquakes with magnitude greater than 4.5 in the last seven days",
-			columns : cols
-		};
+        var tableInfo = {
+            id: "earthquakeFeed",
+            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            columns: cols
+        };
 
-		schemaCallback([tableInfo]);
-	};
+        schemaCallback([tableInfo]);
+    };
 
-	// Download the data
-	myConnector.getData = function (table, doneCallback) {
-		var mag = 0,
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        var mag = 0,
             title = "",
             url = "",
             lat = 0,
             lon = 0;
 
-		$.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function (resp) {
-			var feat = resp.features,
+        $.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+            var feat = resp.features,
                 tableData = [];
 
-			// Iterate over the JSON object
-			for (var i = 0, len = feat.length; i < len; i++) {
-				mag = feat[i].properties.mag;
-				title = feat[i].properties.title;
-				url = feat[i].properties.url;
-				lon = feat[i].geometry.coordinates[0];
-				lat = feat[i].geometry.coordinates[1];
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                mag = feat[i].properties.mag;
+                title = feat[i].properties.title;
+                url = feat[i].properties.url;
+                lon = feat[i].geometry.coordinates[0];
+                lat = feat[i].geometry.coordinates[1];
 
-				tableData.push({
-					"mag" : mag,
-					"title" : title,
-					"url" : url,
-					"lon" : lon,
-					"lat" : lat
-				});
+                tableData.push({
+                    "mag": mag,
+                    "title": title,
+                    "url": url,
+                    "lon": lon,
+                    "lat": lat
+                });
 
-                table.appendRows(tableData);
-			}
+            }
 
-			doneCallback();
-		});
-	};
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
 
-	tableau.registerConnector(myConnector);
+    tableau.registerConnector(myConnector);
 
-	// Create event listeners for when the user submits the form
-	$(document).ready(function () {
-		$("#submitButton").click(function () {
-			tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
-		});
-	});
+        });
+    });
 })();
