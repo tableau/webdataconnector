26277c0575871c5a0f61119cdc878ba5a74809f6
Jared Dominguez <jred51@gmail.com>
2016-06-24 17:04:46 -0700

    Fixes and improvements to multitable earthquake sample

--- Examples/js/earthquakeUSGS.js
+++ Examples/js/earthquakeUSGS.js
@@ -5,6 +5,9 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
+            id: "id",
+            dataType: tableau.dataTypeEnum.string
+        }, {
             id: "mag",
             alias: "magnitude",
             dataType: tableau.dataTypeEnum.float
@@ -13,35 +16,36 @@
             alias: "title",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "url",
-            alias: "url",
-            dataType: tableau.dataTypeEnum.string
-        }, {
             id: "lat",
             alias: "latitude",
+            columnRole: "dimension",
+            // Do not aggregate values as measures in Tableau--makes it easier to add to a map 
             dataType: tableau.dataTypeEnum.float
         }, {
             id: "lon",
             alias: "longitude",
+            columnRole: "dimension",
+            // Do not aggregate values as measures in Tableau--makes it easier to add to a map 
             dataType: tableau.dataTypeEnum.float
         }];
 
-        var tableInfo = {
+        var tableSchema = {
             id: "earthquakeFeed",
             alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
             columns: cols
         };
 
-        schemaCallback([tableInfo]);
+        schemaCallback([tableSchema]);
     };
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        var mag = 0,
+        var id = "",
+            mag = 0,
             title = "",
-            url = "",
             lat = 0,
             lon = 0;
+
 
         $.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
             var feat = resp.features,
@@ -49,16 +53,16 @@
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
+                id = feat[i].id;
                 mag = feat[i].properties.mag;
                 title = feat[i].properties.title;
-                url = feat[i].properties.url;
                 lon = feat[i].geometry.coordinates[0];
                 lat = feat[i].geometry.coordinates[1];
 
                 tableData.push({
+                    "id": id,
                     "mag": mag,
                     "title": title,
-                    "url": url,
                     "lon": lon,
                     "lat": lat
                 });
