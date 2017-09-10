f70b1ef800853005eaae173eb16edfb2d7820b55
albertochavez02 <31809083+albertochavez02@users.noreply.github.com>
2017-09-10 16:21:13 -0500

    Create earthquakeWDC.js

---
+++ earthquakeWDC.js
@@ -0,0 +1,13 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
