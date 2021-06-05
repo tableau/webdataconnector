ee220a776e6a1b575062f481c475164ce3a53c98
Leo Z <3324234+leozusa@users.noreply.github.com>
2021-06-04 20:55:27 -0700

    removing trailing and multiple _

--- assets/js/community_connectors.js
+++ assets/js/community_connectors.js
@@ -1,7 +1,7 @@
 function append(connector) {
     var grid = document.querySelector('#grid');
     var item = document.createElement('div');
-    var elemid = connector.name.replace(/\W/g, '_').replace('__','_').toLowerCase();
+    var elemid = connector.name.replace(/\W/g,' ').trim().replace(/ +/g,'_').toLowerCase();
     var h = '<div id="'+ elemid +'">';
     h += '<div class="thumbnail">';
     h += '<div class="connector_title">';
