542c46a9bf7979e405219e773fb393496d37cfd8
Teng Chen <chenteng2016@hotmail.com>
2021-06-06 15:15:03 -0700

    update cors proxy definition

--- Simulator/utils/corsproxy.js
+++ Simulator/utils/corsproxy.js
@@ -1,20 +1,21 @@
-// defined by env variable or npm scripting
+// define host
 var corsProxyHost = process.env.CORSPROXY_HOST || 'localhost';
-// as for specific values, utilize the same pattern for exiting corsproxy
+// utilize the same pattern for exiting corsproxy package
 var corsProxyPort = process.env.CORSPROXY_PORT || 1337;
 
 var cors_proxy = require('cors-anywhere');
 
 cors_proxy.createServer({
-  // cors proxy allows any domain as origin
+  // the cors proxy allows any domain as origin
   originWhitelist: [],
 
   redirectSameOrigin: true,
 
   // default in no requirements
   // requireHeader: ['origin', 'x-requested-with'],
-  // default in suggested cooki removal settings
+
+  // default in suggested cookie removal settings
   removeHeaders: ['cookie', 'cookie2']
 }).listen(corsProxyPort, corsProxyHost, function() {
-  console.log(`CORS Proxy running at: http://${corsProxyHost}:${corsProxyPort}`);
+  console.log(`CORS proxy running at: http://${corsProxyHost}:${corsProxyPort}`);
 });
