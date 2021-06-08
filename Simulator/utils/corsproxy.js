52e0d797dfc2a33a2fd024e95ad48b7189e0aa07
Teng Chen <chenteng2016@hotmail.com>
2021-06-07 20:10:05 -0700

    update corsproxy configuration

--- Simulator/utils/corsproxy.js
+++ Simulator/utils/corsproxy.js
@@ -1,21 +1,23 @@
-// define host
-var corsProxyHost = process.env.CORSPROXY_HOST || 'localhost';
-// utilize the same pattern for exiting corsproxy package
-var corsProxyPort = process.env.CORSPROXY_PORT || 1337;
+const cors_proxy = require('cors-anywhere');
 
-var cors_proxy = require('cors-anywhere');
+// define proxy host and utilize the same pattern for exiting corsproxy
+const corsProxyHost = process.env.CORSPROXY_HOST || 'localhost';
+
+// define proxy port and utilize the same pattern for exiting corsproxy
+const corsProxyPort = process.env.CORSPROXY_PORT || 1337;
 
 cors_proxy.createServer({
-  // the cors proxy allows any domain as origin
+  // in default the cors proxy allows any domain as origin
   originWhitelist: [],
 
   redirectSameOrigin: true,
 
-  // default in no requirements
-  // requireHeader: ['origin', 'x-requested-with'],
+  // no requirements in default
+  requireHeader: [],
 
-  // default in suggested cookie removal settings
-  removeHeaders: ['cookie', 'cookie2']
+  // no requirements in default
+  // for instance cookie could be added to ensure no cookie is shared
+  removeHeaders: []
 }).listen(corsProxyPort, corsProxyHost, function() {
-  console.log(`CORS proxy running at: http://${corsProxyHost}:${corsProxyPort}`);
+  console.log(`[CORS Proxy] running at: http://${corsProxyHost}:${corsProxyPort}`);
 });
