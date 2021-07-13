bbb5155dbebd3974c6e1d5dbd30cf56273fb8f2d
chenteng2016 <chenteng2016@hotmail.com>
2021-07-13 10:43:54 -0700

    security - fix dependabot alert - ecstatic (#464)
    * trim logs
    
    * add static server for recursive hosting
    
    * update start scripting
    
    * index.js is the server entrypoint
    
    * update packages
    
    * trim des
    
    * update related docs
    
    * trim updates
    
    * use stable
    
    * trim updates
    
    * enable controlling cache setting and trim updates
    
    * ensure printing the serving without spaces
    
    * support cache settings
    
    * ensure logging info

--- Simulator/utils/corsproxy.js
+++ Simulator/utils/corsproxy.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-console */
 const corsProxy = require('cors-anywhere');
 
 // define proxy host and utilize the same pattern for exiting corsproxy
 const corsProxyHost = process.env.CORSPROXY_HOST || 'localhost';
 
 // define proxy port and utilize the same pattern for exiting corsproxy
-const corsProxyPort = process.env.CORSPROXY_PORT || 1337;
+const corsProxyPort = process.env.CORSPROXY_PORT || 8889;
 
 corsProxy.createServer({
   // in default the cors proxy allows any domain as origin
@@ -19,7 +20,6 @@ corsProxy.createServer({
   // for instance cookie could be added to ensure no cookie is shared
   removeHeaders: [],
 }).listen(corsProxyPort, corsProxyHost, () => {
-  /* eslint-disable no-console */
   // log corsproxy's host and port
-  console.info(`[CORS Proxy] running at: http://${corsProxyHost}:${corsProxyPort}`);
+  console.log(`[CORS Proxy] serving at: http://${corsProxyHost}:${corsProxyPort}`);
 });
